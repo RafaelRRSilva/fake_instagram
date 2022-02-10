e0f985de1b3bf61a98588dbdaab63097decc17d4
Sérgio Moura <smouracalmon@gmail.com>
2022-02-10 10:53:54 -0300

    Criando o model Comentario

--- testes/testeModelComentario.js
+++ testes/testeModelComentario.js
@@ -1,8 +1,13 @@
 const { sequelize, Comentario } = require('../models');
 
-Comentario.findAll().then(
-    data => {
-        console.log(data.map( u => u.toJSON()));
+let promessa = Comentario.findAll();
+
+promessa.then(
+    dados => {
+        console.log("Dados chegaram...");
+        console.log("Fechando a conexão com o BD");
         sequelize.close();
     }
-)
+);
+
+console.log("Já foi!");
