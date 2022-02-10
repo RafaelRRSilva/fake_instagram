9e76ddd00bde6dabe5f4aad1a5c49f4e5981188f
Sérgio Moura <smouracalmon@gmail.com>
2022-02-10 14:24:03 -0300

    Alterando o tipo no model Comentario de STRING para TEXT

--- models/Comentario.js
+++ models/Comentario.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
                 allowNull: false
             },
             texto: {
-                type: DataTypes.STRING,
+                type: DataTypes.TEXT,
                 allowNull: false
             },
             usuarios_id: {
