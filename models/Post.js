37ce13251e0d2e6d7aceab2751dae7d8894acfc3
Rafael Silva <rafael.ribeiro.silva@gmail.com>
2022-02-15 10:23:03 -0300

    Atualizando o servidor

---
+++ models/Post.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+    let post = sequelize.define(
+        'Post',
+        {
+            id:{
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                allowNull: false
+            },
+            texto:{
+                type: DataTypes.STRING(100),
+                allowNull: false,
+            },
+            img:{
+                type: DataTypes.STRING(100)
+            },
+            usuarios_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            n_likes: {
+                type: DataTypes.INTEGER
+            }
+        },
+        {
+            tableName: 'posts',
+            timestamps: false
+        }
+    )
+    post.associate = (models) => {
+        post.hasMany(models.Comentario, {foreignKey: 'posts_id', as: 'comentarios'})
+    }
+
+    return post;
+}
